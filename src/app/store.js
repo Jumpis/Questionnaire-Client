2c7352c8bb1c18037bfb6420a40b0a56ff09a06c
Jonghyeon Lee <leejonghyeon0106@gmail.com>
2020-04-13 11:20:12 +0900

    :ok_hand: adding features due to remaking repository

---
+++ src/app/store.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+import { createStore } from 'redux';
+
+export default createStore((state) => state,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
---
+++ src/components/EventEntry.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+export default function EventEntry() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={(
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            3PS
+          </Avatar>
+        )}
+        action={(
+          <IconButton aria-label="settings">
+            <DeleteIcon />
+          </IconButton>
+        )}
+        title="PPPS 런칭이벤트 설문"
+        subheader="April 10, 2020"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          PPPS 런칭이벤트 설문입니다. Made By 김환, 전진철, 머지빌런
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>세부정보 : </Typography>
+          <Typography paragraph>
+            좋아요 수 어쩌구 저쩌구~
+          </Typography>
+          <Typography paragraph>
+            아 리액트 어렵다
+          </Typography>
+          <Typography paragraph>
+            머테리얼 유아이 좋네요dd
+          </Typography>
+          <Typography>
+            배고프다...
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
